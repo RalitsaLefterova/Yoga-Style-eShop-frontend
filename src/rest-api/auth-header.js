9a3667b56d63d3c2ffa9571f178c510b54bd9675
Ralitsa Lefterova <ralitsa.lefterova@gmail.com>
2022-10-24 13:34:10 +0200

    test cors

--- src/rest-api/auth-header.js
+++ src/rest-api/auth-header.js
@@ -1,8 +1,4 @@
 export default authHeader = () => {
   const user = JSON.parse(JSON.parse(localStorage.getItem('persist:root')).user)
-  return user && user.token ? { 
-    Authorization: `Bearer ${user.token}`,
-    'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Methods':'GET,PUT,POST,DELETE,PATCH,OPTIONS'
-  } : {}
+  return user && user.token ? { Authorization: `Bearer ${user.token}` } : {}
 }
