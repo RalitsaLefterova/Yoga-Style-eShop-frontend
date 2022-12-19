245a0bd0058254df87896482711503a718b7389f
Ralitsa Lefterova <ralitsa.lefterova@gmail.com>
2022-12-19 12:14:38 +0100

    return previous store configuration whit configureStore

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,14 +1,12 @@
-// import { configureStore } from '@reduxjs/toolkit'
-import { compose, createStore, applyMiddleware } from 'redux'
+import { configureStore } from '@reduxjs/toolkit'
 import { persistStore } from 'redux-persist'
 import logger from 'redux-logger'
 import createSagaMiddleware from 'redux-saga'
 
-// import { loggerMiddleware } from './middleware/logger'
 import rootReducer from './root-reducer'
 import { rootSaga } from './root-saga'
 
-// const devMode = process.env.NODE_ENV !=='production' && window && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+const devMode = process.env.NODE_ENV !=='production' && window && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
 
 const sagaMiddleware = createSagaMiddleware();
 
@@ -16,28 +14,13 @@ const middleWares = [
   process.env.NODE_ENV !== 'production' && logger,
   sagaMiddleware
 ].filter(Boolean);
-
 // const middlewares = [loggerMiddleware]
 
-// export const store = configureStore({
-//   reducer: rootReducer,
-//   middleware: [...middlewares],
-//   devTools: devMode
-// });
-
-const composeEnhancer = 
-  (process.env.NODE_ENV !=='production' && 
-    window && 
-    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
-  compose;
-
-const composedEnhancers = composeEnhancer(applyMiddleware(...middleWares));
-
-export const store = createStore(
-  rootReducer, 
-  undefined, 
-  composedEnhancers
-);
+export const store = configureStore({
+  reducer: rootReducer,
+  middleware: [...middleWares],
+  devTools: devMode
+});
 
 sagaMiddleware.run(rootSaga)
 
