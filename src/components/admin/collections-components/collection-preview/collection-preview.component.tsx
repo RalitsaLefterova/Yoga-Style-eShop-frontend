336ffdea12b34e743afe66bf588309702251ca8e
Ralitsa Lefterova <ralitsa.lefterova@gmail.com>
2023-08-21 11:50:51 +0200

    fix style in collection's preview

--- src/components/admin/collections-components/collection-preview/collection-preview.component.tsx
+++ src/components/admin/collections-components/collection-preview/collection-preview.component.tsx
@@ -5,6 +5,8 @@ import { useDispatch } from 'react-redux'
 import { deleteCollectionRequested } from 'redux/collections/collections.actions'
 import { Collection } from 'shared/types/collections'
 
+import YogaStyleButton from 'components/custom-components/yoga-style-button/yoga-style-button.component'
+
 import './collection-preview.style.scss'
 
 type PreviewCollectionType = {
@@ -34,21 +36,19 @@ const CollectionPreview = ({ collection, callbackAfterDelete }: PreviewCollectio
   }
 
   return (
-    <div>
-      <div>
-        <h4>Collection Preview</h4>
+    <div className='collection-preview-container'>
+      <div className='section-title center'>
+        <h2>Collection Preview</h2>
       </div>
-      <div>
-        {collection.title}
+      <div className='capitalize-text padding-top-bottom-20'>
+        <h1>"{collection.title}"</h1>
       </div>
-      <div className='collection-image-box'>
+      <div className='collection-image-box center'>
         <img src={`${process.env.BACKEND_URL}/${cover}`} alt={`collection ${title}`} />
       </div>
-      <div>
-        <button onClick={handleStartEditing}>Edit</button>
-      </div>
-      <div>
-        <button onClick={handleDelete}>Delete</button>
+      <div className='buttons-container center'>
+        <YogaStyleButton onClick={handleStartEditing} >Edit</YogaStyleButton>
+        <YogaStyleButton onClick={handleDelete} extraClasses='delete-btn-style' >Delete</YogaStyleButton>
       </div>
     </div>
   )
